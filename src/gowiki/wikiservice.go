bd1c01041e206cf99dde532ca40c053b2ce238e6
Vivek Kumar Singh <vivekkumarsingh@olacabs.com>
2017-01-20 16:43:27 +0530

    Adding changes after templates, error handling, validation and closure

--- src/gowiki/wikiservice.go
+++ src/gowiki/wikiservice.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
+
+var templates = template.Must(template.ParseFiles("view.html", "edit.html"))
+var validPaths = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 //Page struct
 type Page struct {
 	Title string
 	Body  []byte
+}
+
+func findTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPaths.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
 }
 
 func (p *Page) save() error {
@@ -26,39 +41,63 @@ func loadPage(title string) (*Page, error) {
 	return &Page{title, body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
-		fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", "Error", err)
-	} else {
-		fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+		p = &Page{title, []byte(err.Error())}
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
 	}
+	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing %s </h1><form action=\"/save/%s\"><textarea name=\"body\">%s</textarea><input type=\"submit\" value=\"Save\"/></form>", p.Title, p.Title, p.Body)
+	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func renderTemplate(w http.ResponseWriter, templateName string, p *Page) {
+	t, err := template.ParseFiles(templateName + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	err = t.ExecuteTemplate(w, templateName+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{title, []byte(body)}
-	p.save()
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func makeHandler(fn func(w http.ResponseWriter, r *http.Request, title string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPaths.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("Error while starting server", err)
+		fmt.Println("Error while starting server", err.Error())
 	}
 }
